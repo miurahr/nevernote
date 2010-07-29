77f42518d299117e5f83b345107ad3196e006012
Hiroshi Miura (momokuri) <miurahr@linux.com>
2010-07-29 11:54:48 +0900

    Launch script now tune for performance
       add several JVM options for performace
       add debug options

--- nevernote.bat
+++ nevernote.bat
@@ -2,8 +2,8 @@ rem #####################
 rem # Install variables #
 rem #####################
 set NEVERNOTE=C:\NeverNote
-set JAMBI_LOCATION=C:\qtjambi-win32-lgpl-4.5.0_01
-set JAMBI_VERSION=4.5.0_01
+set JAMBI_LOCATION=C:\qtjambi-win32-lgpl-4.5.2_01
+set JAMBI_VERSION=4.5.2_01
 set JAMBI_PLATFORM=win32-msvc2005
 
 
@@ -22,10 +22,27 @@ rem # that say "out of memory" you need    #
 rem # to increase these values.            #
 rem ########################################
 rem # Initial heap size
-set NN_XMS=128M
+set NN_XMS=256M
 rem # Maximum heap size
 set NN_XMX=512M
 
+rem ## The young generation
+rem # the young generation will occupy 1/2 of total heap
+set NN_NEW_RATIO=1
+
+rem ## GC option
+rem ## recommend Incremental Low Pause GC for desktop apps 
+set NN_GC_OPT=-Xincgc
+rem ## recent multi-core CPU may show good performance
+rem set NN_GC_OPT=-XX:+UseParNewGC
+rem set NN_GC_OPT=-XX:+UseConcMarkSweepGC
+rem ## same as default
+rem set NN_GC_OPT=-XX:+UseParallelGC
+
+rem ## debug
+rem set NN_DEBUG=-agentlib:hprof=format=b
+rem set NN_DEBUG=-agentlib:hprof=cpu=samples,format=a
+rem set NN_DEBUG=-verbose:gc 
 
 rem ########################################
 rem # This next variable is optional. It   #
@@ -61,7 +78,8 @@ set NN_CLASSPATH=%NN_CLASSPATH%;%NEVERNOTE%\lib\log4j-1.2.14.jar
 set NN_CLASSPATH=%NN_CLASSPATH%;%NEVERNOTE%\lib\h2-1.2.136.jar
 set NN_CLASSPATH=%NN_CLASSPATH%;%NEVERNOTE%\lib\PDFRenderer.jar
 set NN_CLASSPATH=%NN_CLASSPATH%;%JAMBI_LOCATION%\qtjambi-%JAMBI_VERSION%.jar
+set NN_CLASSPATH=%NN_CLASSPATH%;%JAMBI_LOCATION%\qtjambi-util-%JAMBI_VERSION%.jar
 set NN_CLASSPATH=%NN_CLASSPATH%;%JAMBI_LOCATION%\qtjambi-%JAMBI_PLATFORM%-%JAMBI_VERSION%.jar
 
-start /B javaw -Xmx%NN_XMX% -Xms%NN_XMS%  -classpath %NN_CLASSPATH% cx.fbn.nevernote.NeverNote --name=%NN_NAME%
+start /B javaw -Xmx%NN_XMX% -Xms%NN_XMS%  -XX:NewRatio=$NN_NEW_RATIO $NN_GC_OPT $NN_DEBUG  -classpath %NN_CLASSPATH% cx.fbn.nevernote.NeverNote --name=%NN_NAME%
 exit

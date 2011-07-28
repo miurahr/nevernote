82a6fc3bcf02c3425ab8413cb77b10c8c4453b53
Randy Baumgarte <randy@fbn.cx>
2011-07-28 11:31:48 -0400

    Remove unused variable.

--- src/cx/fbn/nevernote/sql/NoteResourceTable.java
+++ src/cx/fbn/nevernote/sql/NoteResourceTable.java
@@ -37,7 +37,7 @@ public class NoteResourceTable  {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+//	private static final long serialVersionUID = 1L;
 	private final ApplicationLogger 		logger;
 	private final DatabaseConnection 		db;	
 	

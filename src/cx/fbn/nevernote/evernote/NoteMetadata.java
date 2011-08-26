fe6277b043aa5be90c25f75f224fc97c3533f515
Randy Baumgarte <randy@fbn.cx>
2011-08-26 16:56:16 -0400

    Add missing NoteMetadata class.

---
+++ src/cx/fbn/nevernote/evernote/NoteMetadata.java
@@ -0,0 +1,48 @@
+package cx.fbn.nevernote.evernote;
+
+import com.trolltech.qt.gui.QColor;
+
+public class NoteMetadata {
+	private String guid;
+	private int color;
+	private boolean pinned;
+	private boolean dirty;
+	
+	public NoteMetadata() {
+		color = QColor.white.rgb();
+		pinned = false;
+		dirty = false;
+	}
+	
+	public String getGuid() {
+		return guid;
+	}
+	public void setGuid(String g) {
+		guid = g;
+	}
+	
+	public int getColor() {
+		return color;
+	}
+	public void setColor(int c) {
+		color = c;
+	}
+	public void setPinned(boolean value) {
+		pinned = value;
+	}
+	public boolean isPinned() {
+		return pinned;
+	}
+	public void setDirty(boolean v) {
+		dirty = v;
+	}
+	public boolean isDirty() {
+		return dirty;
+	}
+	public void copy(NoteMetadata m) {
+		color = m.getColor();
+		guid = m.getGuid();
+		pinned = m.isPinned();
+		dirty = m.isDirty();
+	}
+}
